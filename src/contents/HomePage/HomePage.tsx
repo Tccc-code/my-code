92c7249e18773415f267282dab577f19e6941924
刘容廷 <tccc@10.0.0.7>
2023-02-24 00:06:42 +0800

    提交

--- src/contents/HomePage/HomePage.tsx
+++ src/contents/HomePage/HomePage.tsx
@@ -5,6 +5,7 @@ import './HomePage.scss'
 const HomePage: React.FC<{}> = () => {
   return (
     <div className="home-page">
+      <div>HomePage</div>
       <div className="icp">
         <a href="https://beian.miit.gov.cn/" target="_blank">浙ICP备2023003826号</a>
       </div>
